03dcc4e043d84832b2c11160853bd945dd168b17
SeongJin Bae <tjdwlslrj@naver.com>
2022-01-13 01:38:48 +0900

    upload 2022/01/13

---
+++ BOJ/Python/[Python]BOJ_2444.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for _ in range(n-i-1):
+        print(' ', end='')
+    for _ in range(2*i+1):
+        print('*', end = '')
+    print()
+for i in reversed(range(n-1)):
+    for _ in range(n-i-1):
+        print(' ', end='')
+    for _ in range(2*i+1):
+        print('*', end = '')
+    print()
