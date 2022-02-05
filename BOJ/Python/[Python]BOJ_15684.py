549864a5130df5f8271b521e6e8e491435db9317
SeongJin Bae <tjdwlslrj@naver.com>
2022-02-05 16:40:31 +0900

    update 2022/02/05

--- BOJ/Python/[Python]BOJ_15684.py
+++ BOJ/Python/[Python]BOJ_15684.py
@@ -2,82 +2,60 @@
 
 #15684 사다리 조작.py
 
-global x, y
-x, M, y = map(int, input().split())
-matrix = [[0]*(x+1) for _ in range(y+2)]
-stair = [0]*(x+1)
-for _ in range(M):
-    i, j = map(int, input().split())
-    stair[j] += 1
-    matrix[i][j] = j
-    matrix[i][j+1] = j
-
-
-for j in range(y+1):
-    print(matrix[j])
-def check_start(init_stair):
-    cnt = 0
-    result = []
-    for idx, s in enumerate(init_stair):
-        if cnt > 3: return 0
-        if s % 2: 
-            cnt += 1
-            result.append(idx)
+def make_map():
+    result = [[0]*(x+1) for _ in range(y+2)]
+    for _ in range(M):
+        i, j = map(int, input().split())
+        result[i][j] = j
+        result[i][j+1] = j
     return result
 
+
+def find():
+    result = []
+    for i in range(1, x):
+        for j in range(1, y+1):
+            if matrix[j][i] == 0 and matrix[j][i+1] == 0:
+                result.append([i, j])
+    return result
+
+
 def down():
-    global x, y
-    
-        
-    for i in range(1, x + 1):
-        sx, sy = i, 0
-        print(f'\nSTART [{i}, 0] ====')
-        for j in range(y+1):
-            print(matrix[j])
+    for i in range(1, x):
+        sx = i
+        for j in range(1, y + 1):
+            if matrix[j][sx]:
+                if sx + 1 <= x and matrix[j][sx + 1] == matrix[j][sx]:
+                    sx += 1
+                elif matrix[j][sx - 1] == matrix[j][sx]:
+                    sx -= 1
 
-
-        while True:
-            nx, ny = sx, sy + 1
-            # print(nx, ny)
-
-            if sy == y + 1:
-                # print(sx, i, sy, y+1)
-                if sx == i: break
-                else: return False
-
-            if matrix[ny][nx] != 0:
-                if nx + 1 <= x and matrix[ny][nx + 1] == matrix[ny][nx]:
-                    sx = nx + 1
-                elif 0 < nx - 1 and matrix[ny][nx - 1] == matrix[ny][nx]:
-                    sx = nx - 1
-            sy = ny
-
-        print(f'END    [{i}] ====')
+        if sx != i: return False
     return True
 
-def dfs(cnt):
-    if len(point) == 0:
-        if down():
-            print(cnt)
-            return True
-        return False
-    p = point.pop()
-    for i in range(1, y+1):
-        if matrix[i][p] == 0 and matrix[i][p+1] == 0:
-            matrix[i][p] = matrix[i][p+1] = p
-            print(f'APPEND [{p}, {i}] and [{p+1}, {i}]')
-            if dfs(cnt + 1):
-                return True
-            matrix[i][p] = matrix[i][p+1] = 0
-    point.append(p)
+
+def dfs(cnt, idx, MIN):
+    if cnt == MIN:
+        return down()
+
+    for i in range(idx, len(put_list)):
+        nx, ny = put_list[i]
+        if matrix[ny][nx] == 0 and matrix[ny][nx+1] == 0:
+            matrix[ny][nx] = matrix[ny][nx+1] = nx
+            if dfs(cnt + 1, idx + 1, MIN): return True
+            matrix[ny][nx] = matrix[ny][nx+1] = 0
 
 
+if __name__ == '__main__':
+    x, M, y = map(int, input().split())
+    if M == 0: print(0); exit()
+    matrix = make_map()
+    put_list = find()
+    result = -1
 
+    for i in range(4):
+        if dfs(0, 0, i):
+            result = i
+            break
 
-point = check_start(stair)
-print(point)
-if type(point) == list:
-    if not dfs(0):
-        print(-1)
-else:
-    print(-1)
+    print(result)
