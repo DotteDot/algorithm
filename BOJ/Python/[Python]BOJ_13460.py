b27982389cabbea8d8d7d36164e349aa0a1edcad
SeongJin Bae <tjdwlslrj@naver.com>
2022-03-05 20:40:36 +0900

    update 2022/03/05

--- BOJ/Python/[Python]BOJ_13460.py
+++ BOJ/Python/[Python]BOJ_13460.py
@@ -2,13 +2,36 @@
 
 #13460 구슬 탈출 2.py
 
-# r_loc: r의 위치, b_loc: b의 위치
-# direction: 동남서북( [[1,0],[0,1],[-1,0],[0,-1]] )
-# 0 <= cnt < 11
+'''
+4군데 다 돌린다.
+
+만약에 1도 안움직인다? return하려고 했으나 하면 안된다! 그냥 다 돌리자!
+<반례>
+#######
+#BR...#
+#.#####
+#.....#
+#.###.#
+#####.#
+#O....#
+#######
+
+첫줄은 세로, 가로 순서이다.
+
+dfs(r_loc, b_loc, direction, cnt)
+r_loc: r의 위치
+b_loc: b의 위치
+direction: 동남서북( [[1,0],[0,1],[-1,0],[0,-1]] )
+0 <= cnt < 10
+
+더 줄이는 방법
+min보다 크면 바로 break
+'''
+
 def dfs(r_loc, b_loc, direction, cnt):
     global MIN
 
-    if cnt > 10:
+    if cnt >= MIN:
         return
 
     r_sx, r_sy = r_loc[0], r_loc[1]
@@ -20,23 +43,22 @@ def dfs(r_loc, b_loc, direction, cnt):
         b_nx, b_ny = b_sx + direction[0], b_sy + direction[1]
 
         flag = 0
+        # R 옮기기
         if 1 <= r_nx < n[0]-1 and 1 <= r_ny < n[0]-1:
             if (r_nx, r_ny) != (b_sx, b_sy):
                 if mat[r_ny][r_nx] == '.':
-                    r_sx = r_nx
-                    r_sy = r_ny
+                    r_sx, r_sy = r_nx, r_ny
                     flag += 1
                 elif mat[r_ny][r_nx] == 'O':
                     out = cnt
-                    r_sx = -10
-                    r_sy = -10
+                    r_sx = r_sy = -10
                     flag += 1
 
+        # B 옮기기
         if 1 <= b_nx < n[0]-1 and 1 <= b_ny < n[0]-1:
             if (b_nx, b_ny) != (r_sx, r_sy):
                 if mat[b_ny][b_nx] == '.':
-                    b_sx = b_nx
-                    b_sy = b_ny
+                    b_sx, b_sy = b_nx, b_ny
                     flag += 1
                 elif mat[b_ny][b_nx] == 'O':
                     return
@@ -69,7 +91,4 @@ for i in range(n[1]):
 for i in d:
     dfs(r_loc, b_loc, i, 1)
 
-if MIN == 11:
-    print(-1)
-else:
-    print(MIN)
+print(-1 if MIN == 11 else MIN)
